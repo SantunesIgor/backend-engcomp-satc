0e1fb5111ec2a0a442aac7d0e9339c959aaa23f1
SantunesIgor <igorsantunes@outlook.com>
2025-09-28 17:32:36 -0300

    Lista 2 refeita

---
+++ Lista2/src/main/java/org/example/GeradorDePokemon.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GeradorDePokemon {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner scan = new Scanner(System.in);
+
+        ArrayList<String> nomes = new ArrayList<>(Arrays.asList(
+                "Pikachu",
+                "Charmander",
+                "Bulbasaur",
+                "Squirtle",
+                "Jigglypuff",
+                "Meowth",
+                "Psyduck",
+                "Eevee",
+                "Snorlax",
+                "Gengar"
+        ));
+
+        ArrayList<String> tipos = new ArrayList<>(Arrays.asList(
+                "Normal",
+                "Fogo",
+                "Água",
+                "Elétrico",
+                "Grama",
+                "Gelo",
+                "Lutador",
+                "Venenoso",
+                "Terrestre",
+                "Voador",
+                "Psíquico",
+                "Inseto",
+                "Pedra",
+                "Fantasma",
+                "Dragão",
+                "Sombrio",
+                "Aço",
+                "Fada"
+        ));
+
+        int nivel = rand.nextInt(99) + 1;
+
+        System.out.print("Digite a quantidade de pokémons que serão gerados: ");
+        int quant = scan.nextInt();
+
+        for(int i = 0; i < quant; i++) {
+            System.out.println(
+                    nomes.get(rand.nextInt(nomes.size())) +
+                            " é um Pokémon do tipo " +
+                            tipos.get(rand.nextInt(tipos.size())) +
+                            " de nível " +
+                            nivel + "."
+            );
+        }
+    }
+}
---
+++ Lista2/src/main/java/org/example/GeradorDeSenhas.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GeradorDeSenhas {
+    public static void main(String[] args) {
+        // Também seria possível obter os chars usando o método .charAt(index)
+        Random rand = new Random();
+
+        String charString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        ArrayList<Character> charList = new ArrayList<>();
+        StringBuilder senha = new StringBuilder();
+
+        for (char c : charString.toCharArray()) {
+            charList.add(c);
+        }
+
+        while (senha.length() < 8) {
+            senha.append(charList.get(rand.nextInt(charList.size())));
+        }
+
+        System.out.println(senha);
+    }
+}
+
