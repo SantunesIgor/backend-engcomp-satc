38e188786148c6c5b75e8f629b57711d70553024
Ígor Da Silva Antunes <igor.39012@alunosatc.edu.br>
2025-09-29 19:54:36 -0300

    Lista 3 finalizada

--- Lista 3/src/main/java/org/example/data/Data.java
+++ Lista 3/src/main/java/org/example/data/Data.java
@@ -1,16 +1,65 @@
 package org.example.data;
 
+import java.time.Year;
+
 public class Data {
     private int mes;
     private int dia;
     private int ano;
 
     public Data(int mes, int dia, int ano) {
-        if(mes < 0 || mes > 12){
-            throw new IllegalArgumentException("mes invalido");
-        } else {
-            this.mes = mes;
-
+        if(dia < 1 || dia > quantDias(mes, ano)) {
+            throw new IllegalArgumentException("Dia inválido!");
+        }
+        this.mes = mes;
+        this.dia = dia;
+        this.ano = ano;
     }
 
+    private int quantDias(int mes, int ano) {
+        switch (mes) {
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+                return 31;
+            case 4: case 6: case 9: case 11:
+                return 30;
+            case 2:
+                return Year.isLeap(ano) ? 29 : 28;
+            default:
+                throw new IllegalArgumentException("Mês inválido");
+        }
+    }
+
+    public void displayData(){
+        System.out.println(this.dia + "/" + this.mes + "/" + this.ano);
+    }
+
+    public int getMes() {
+        return this.mes;
+    }
+
+    public void setMes(int mes) {
+        if(mes < 1 || mes < 12){
+            throw new IllegalArgumentException("Mês inválido");
+        }
+        this.mes = mes;
+    }
+
+    public int getDia() {
+        return this.dia;
+    }
+
+    public void setDia(int dia) {
+        if(dia < 1 || dia > quantDias(this.mes, this.ano)) {
+            throw new IllegalArgumentException("Dia inválido!");
+        }
+        this.dia = dia;
+    }
+
+    public int getAno() {
+        return this.ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
 }
--- Lista 3/src/main/java/org/example/data/DataTeste.java
+++ Lista 3/src/main/java/org/example/data/DataTeste.java
@@ -2,6 +2,18 @@ package org.example.data;
 
 public class DataTeste {
     public static void main(String[] args) {
+        Data d1 = new Data(12, 25, 314);
+        Data d2 = new Data(8, 6, 2006);
+        Data d3 = new Data(5, 19, 2001);
 
+        d1.displayData();
+        d2.displayData();
+        d3.displayData();
+
+        // Data dErroBissexto = new Data(29, 1, 1111);
+        // Data dErroDiaMaior = new Data(1, 32, 1111);
+        // Data dErroDiaMenor = new Data(1, 0, 1111);
+        // Data dErroMesMaior = new Data(13, 1, 1111);
+        // Data dErroMesMenos = new Data(0, 1, 1111);
     }
 }
